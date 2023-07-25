5812eb6161ab86185262fd8e16cfc9916d3fcb0c
David24-rea <wilmer.rea.2821@utc.edu.ec>
2023-07-25 16:10:22 -0500

    carpeta lib

--- lib/vistas/azucares.dart
+++ lib/vistas/azucares.dart
@@ -12,7 +12,7 @@ class _azucaresLState extends State<azucaresL> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("LIstado"),
+        title: Text("LIstado de productos azucarados"),
       ),
     );
   }
